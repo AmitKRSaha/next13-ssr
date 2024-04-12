fc45a6ca6b39dcf115389cb1374193e5b3ad2be1
herbalamit1990 <amitsa@herbalife.com>
2024-04-12 16:14:47 +0530

    download dynamic component on scroll

--- pages/dynamic-load-on-scroll.tsx
+++ pages/dynamic-load-on-scroll.tsx
@@ -1,12 +1,75 @@
-import React, {useRef, useState, useEffect} from "react";
+import React, { useRef, useState, useEffect, ReactElement, RefObject } from "react";
 import dynamic from 'next/dynamic';
 
 import styles from '@/styles/DynamicLoad.module.css';
-import RenderOnViewportEntry from "@/components/HOC/RenderOnViewportEntry";
+import Bear from "../components/Bear";
 
 const DynamicBear = dynamic(() => import('../components/Bear'), {
     ssr: false,
 })
+
+interface Args extends IntersectionObserverInit {
+    freezeOnceVisible?: boolean;
+}
+
+function useIntersectionObserver(
+    elementRef: RefObject<Element>,
+    {
+        threshold = 0,
+        root = null,
+        rootMargin = "0%",
+        freezeOnceVisible = false,
+    }: Args
+): IntersectionObserverEntry | undefined {
+    const [entry, setEntry] = useState<IntersectionObserverEntry>();
+
+    const frozen = entry?.isIntersecting && freezeOnceVisible;
+
+    const updateEntry = ([entry]: IntersectionObserverEntry[]): void => {
+        setEntry(entry);
+    };
+
+    const stableThreshold = Array.isArray(threshold)
+        ? threshold.join(";")
+        : threshold;
+
+    useEffect(() => {
+        const node = elementRef?.current;
+        const hasIOSupport = !!window.IntersectionObserver;
+
+        if (!hasIOSupport || frozen || !node) return;
+
+        const inputThreshold =
+            typeof stableThreshold === "string"
+                ? stableThreshold.split(";").map(Number)
+                : stableThreshold;
+
+        const observerParams = { threshold: inputThreshold, root, rootMargin };
+        const observer = new IntersectionObserver(updateEntry, observerParams);
+
+        observer.observe(node);
+
+        return () => observer.disconnect();
+    }, [elementRef, stableThreshold, root, rootMargin, frozen]);
+
+    return entry;
+}
+
+const RenderWithWindowIntersection = ({
+    children,
+}: {
+    children: ({ visible }: { visible: boolean }) => ReactElement;
+}) => {
+    const ref = useRef<HTMLDivElement | null>(null);
+    const entry = useIntersectionObserver(ref, {});
+    const visible = Boolean(entry?.isIntersecting);
+
+    return (
+        <div ref={ref}>
+            <>{children({ visible })}</>
+        </div>
+    );
+};
 
 const DynamicLoad = () => {
 
@@ -45,12 +108,13 @@ const DynamicLoad = () => {
             <main ref={ref} className={`${styles.Main}`} >
                 <div className={`${styles.childOne} ${styleVal ? styles.slideIn : ''}`}>Child One</div>
                 <div className={`${styles.childTwo} ${styleVal ? styles.slideIn : ''}`}>Child Two</div>
-                
-                {/* {styleVal ?? <DynamicBear />} */}
-                {/* <RenderOnViewportEntry threshold={0.25} className={`${styles.slideIn}`}>
-                    <DynamicBear />
-                </RenderOnViewportEntry> */}
+
             </main>
+            <RenderWithWindowIntersection>
+                {({ visible }) => {
+                    return <>{visible ? <DynamicBear /> : null}</>;
+                }}
+            </RenderWithWindowIntersection>
             <footer className={`${styles.Footer}`} >This is the Footer</footer>
         </div>
     )
